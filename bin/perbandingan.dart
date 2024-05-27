28a6de0886f01770104dc448713b9584427e9537
Andi Rustianto <anditodev46@gmail.com>
2024-05-28 00:59:22 +0700

    Operator perbandingan

---
+++ bin/perbandingan.dart
@@ -0,0 +1,12 @@
+void main() {
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print("Andi" == "andi");
+  print("Andi" == "Andi");
+  print("Andi" != "Andi");
+}
