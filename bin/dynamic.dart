ac4a6af97010f307950867ea7d9c90f39b4a8bf1
Andi Rustianto <anditodev46@gmail.com>
2024-05-28 10:14:06 +0700

    tipe data dynamic

--- bin/dynamic.dart
+++ bin/dynamic.dart
@@ -1,4 +1,8 @@
 void main() {
+  // dynamic
+  // Kadang ada kebutuhan kita ingin membuat variable yang bisa menampung semua jenis tipe data
+  // Pada kasus ini, kita bisa menggunakan tipe data dynamic
+
   dynamic exVar = 199;
   print(exVar);
 
