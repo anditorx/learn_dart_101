01621abe25ec3c648a69db91026321c46688f615
Andi Rustianto <anditodev46@gmail.com>
2024-05-28 10:50:01 +0700

    set

--- bin/set.dart
+++ bin/set.dart
@@ -3,16 +3,13 @@ void main() {
    * Set merupakan tipe data sama seperti List, namun ada beberapa hal yang berbeda dengan List
    * Set tidak menerima duplikat data, artinya jika kita memasukkan data duplikat, hanya satu yang diterima, yang lainnya akan dihiraukan
    * Set tidak menjamin urutan data, jika dalam List, urutan data sudah pasti menggunakan index, pada Set tidak ada index
-   * 
-   * 
    */
-
   Set<int> numbers = {};
   var strings = <String>{};
   final doubles = <double>{};
   print(numbers);
 
-  var names = <String>{};
+  var names = <String>{"Andi", "Rustianto"};
   names.add("Arhan");
   names.add("Bayu");
   names.add("Charly");
