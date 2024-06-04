952ef9299c0a8677f8e9eb14b943d6dd9329aa56
Andi Rustianto <anditodev46@gmail.com>
2024-06-04 15:24:19 +0700

    Symbol

---
+++ bin/symbol.dart
@@ -0,0 +1,16 @@
+void main () {
+/*
+Symbol
+-
+Symbol merupakan tipe data yang sebenarnya jarang sekali digunakan
+Symbol sebenarnya bisa digunakan sebagai constant
+Untuk membuat Symbol, kita bisa menggunakan tanda #, atau jika kita butuh membuat Symbol dengan nama yang mengandung spasi, kita bisa gunakan Symbol(“text”)
+
+* */
+
+  Symbol symbol1 = Symbol("Andi Rustianto");
+  var symbol2 = "#belajar";
+
+  print(symbol1);
+  print(symbol2);
+}
