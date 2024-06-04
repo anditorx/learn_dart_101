6abcaa4113e9611f053f0bd01ab041226ea14e73
Andi Rustianto <anditodev46@gmail.com>
2024-06-04 22:11:30 +0700

    ternary operator in dart

---
+++ bin/ternaryOperator.dart
@@ -0,0 +1,12 @@
+void main() {
+  /*
+  * Ternary operator adalah operator sederhana dari if
+    Ternary operator terdiri dari  kondisi yang dievaluasi,
+    jika menghasilkan true maka nilai pertama diambil, jika false, maka nilai kedua diambil
+  * */
+
+  var nilai = 75;
+  var ucapan = nilai >= 75 ? "Selamat anda lulus" : "Silahkan coba lagi";
+
+  print(ucapan);
+}
